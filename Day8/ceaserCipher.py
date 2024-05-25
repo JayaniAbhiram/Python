5805ae5fbeadfe2ba2ba8003ef5a3610bc514345
“Jayani <jayaniabhiram@gmail.com>
2024-05-25 21:34:46 +0530

    day8

--- Day8/ceaserCipher.py
+++ Day8/ceaserCipher.py
@@ -1,18 +1,55 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("type encrypt or decrypt of ur choce: ")
+from ceaserCipherArt import logo
+print(logo)
 
-text = input("enter the text you wanna pass: ").lower()
-shift = int(input("enter hoiw may char u wanna shift: "))
+def ceaser():
+    direction = input("type encode or decode of ur choce: ")
+
+    text = input("enter the text you wanna pass: ").lower()
+    shift = int(input("enter hoiw may char u wanna shift: "))
+    shift = shift % 26
+    if direction == "encode":
+        cipherText = ""
+        for letter in text:
+            if letter not in alphabet:
+                letter = letter
+                cipherText = cipherText + letter
+            else:
+                position = alphabet.index(letter)
+                newPosition = position+ shift
+                newLetter = alphabet[newPosition]
+                cipherText = cipherText + newLetter
+        print("the encoded text is :" + cipherText)
+    elif(direction == "decode"):
+        cipherText = ""
+        for letter in text:
+            if letter not in alphabet:
+                letter = letter
+                cipherText = cipherText + letter
+            else:
+                position = alphabet.index(letter)
+                newPosition = position - shift
+                newLetter = alphabet[newPosition]
+                cipherText = cipherText + newLetter
+        print("the decoded text is: " + cipherText)
+    else:
+        print("choose either encode or decode")
 
 
-def encrypt(text,shift):
-    for letter in text:
-        position = alphabet.index(letter)
-        newPosition = position+ shift
+ceaser()
+
+end = False
+
+while not end:     
+    # ceaser()
+    nextStep = input("choose continue to continue and bye to end: ")
+    if nextStep == "bye":
+        end = True
+    else:
+        ceaser()
+
     
-
-
-encrypt(text,shift)
-
+    
+print("game over")
 

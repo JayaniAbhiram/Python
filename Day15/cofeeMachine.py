d44ae0aaffb1ecb00a55b8233608f1c82168dbb0
“Jayani <jayaniabhiram@gmail.com>
2024-05-26 22:03:48 +0530

    15

---
+++ Day15/cofeeMachine.py
@@ -0,0 +1,42 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+end = False
+while not end:
+    userInput = input("what do u wanna drink ..??(espresso/latte/cappuccino): ")
+    if userInput == "off":
+        end = True
+
+    elif userInput == "report":
+        report = resources["water","milk","coffee"]
+        print(report)
+    
+
